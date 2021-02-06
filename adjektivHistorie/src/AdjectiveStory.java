b61e26144a295c0c5229a808a4b97fae553eca22
KidRonee <mabr075@student.kristiania.no>
2021-02-06 16:46:36 +0100

    added changes

--- adjektivHistorie/src/AdjectiveStory.java
+++ adjektivHistorie/src/AdjectiveStory.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -76,8 +77,11 @@ public class AdjectiveStory {
         ArrayList<Integer> indexes = new ArrayList<Integer>();
         FileReader reader = new FileReader("C:\\Users\\Mathi\\Desktop\\story.txt");
         Scanner scanStory = new Scanner(reader);
+        System.out.print("Please select a name for your file (i'll take care of the file type (:\n>");
+        String fileName = createTextFile();
+        String fileLocation = "C:\\Users\\Mathi\\Desktop\\" + fileName + ".txt";
         BufferedWriter writer = new BufferedWriter(
-                new FileWriter("C:\\Users\\Mathi\\Desktop\\modifiedStory.txt")
+                new FileWriter(fileLocation)
         );
 
         while(scanStory.hasNextLine()){
@@ -89,46 +93,57 @@ public class AdjectiveStory {
             for(String word : words){
                 Random index = new Random();
                 if(word.equals("__")){
-                    System.out.println("Please select a number from the list below to replace '__'");
-                    for (i = 0; i <= 10; i++) {
-                        int randomIndex = index.nextInt(adjectives.size());
-                        System.out.println("#" + i + " " + adjectives.get(randomIndex));
-                        indexes.add(randomIndex);
-                    }
-                    System.out.println(currentLine);
+                    System.out.println("#1 to choose your own adjective\n#2 for a prefixed list of adjectives");
                     switch (input.nextInt()) {
                         case 1:
-                            sentences.append(adjectives.get(indexes.get(0))).append(" ");
+                            System.out.println(currentLine);
+                            System.out.print("Type your adjective here: >");
+                            input.nextLine(); //flushing linefeed
+                            sentences.append(input.nextLine().toLowerCase()).append(" ");
                             break;
                         case 2:
-                            sentences.append(adjectives.get(indexes.get(1))).append(" ");
-                            break;
-                        case 3:
-                            sentences.append(adjectives.get(indexes.get(2))).append(" ");
-                            break;
-                        case 4:
-                            sentences.append(adjectives.get(indexes.get(3))).append(" ");
-                            break;
-                        case 5:
-                            sentences.append(adjectives.get(indexes.get(4))).append(" ");
-                            break;
-                        case 6:
-                            sentences.append(adjectives.get(indexes.get(5))).append(" ");
-                            break;
-                        case 7:
-                            sentences.append(adjectives.get(indexes.get(6))).append(" ");
-                            break;
-                        case 8:
-                            sentences.append(adjectives.get(indexes.get(7))).append(" ");
-                            break;
-                        case 9:
-                            sentences.append(adjectives.get(indexes.get(8))).append(" ");
-                            break;
-                        case 10:
-                            sentences.append(adjectives.get(indexes.get(9))).append(" ");
-                            break;
-                        default:
-                            System.out.println("invalid number...");
+                            System.out.println("Please select a number from the list below to replace '__'");
+                            for (i = 0; i <= 10; i++) {
+                                int randomIndex = index.nextInt(adjectives.size());
+                                System.out.println("#" + i + " " + adjectives.get(randomIndex));
+                                indexes.add(randomIndex);
+                            }
+                            System.out.println(currentLine);
+                            switch (input.nextInt()) {
+                                case 1:
+                                    sentences.append(adjectives.get(indexes.get(0))).append(" ");
+                                    break;
+                                case 2:
+                                    sentences.append(adjectives.get(indexes.get(1))).append(" ");
+                                    break;
+                                case 3:
+                                    sentences.append(adjectives.get(indexes.get(2))).append(" ");
+                                    break;
+                                case 4:
+                                    sentences.append(adjectives.get(indexes.get(3))).append(" ");
+                                    break;
+                                case 5:
+                                    sentences.append(adjectives.get(indexes.get(4))).append(" ");
+                                    break;
+                                case 6:
+                                    sentences.append(adjectives.get(indexes.get(5))).append(" ");
+                                    break;
+                                case 7:
+                                    sentences.append(adjectives.get(indexes.get(6))).append(" ");
+                                    break;
+                                case 8:
+                                    sentences.append(adjectives.get(indexes.get(7))).append(" ");
+                                    break;
+                                case 9:
+                                    sentences.append(adjectives.get(indexes.get(8))).append(" ");
+                                    break;
+                                case 10:
+                                    sentences.append(adjectives.get(indexes.get(9))).append(" ");
+                                    break;
+                                default:
+                                    System.out.println("invalid number...");
+                            }
+                        break;
                     }
                 }
                 else {
@@ -136,32 +151,21 @@ public class AdjectiveStory {
                 }
                 indexes = new ArrayList<Integer>();
             }
-            writer.write(sentences + "\n");
+
+
+            System.out.println(sentences);
+            writer.write(String.valueOf(sentences + "\n"));
         }
         reader.close();
         writer.close();
     }
-
-
-
 
     public String createTextFile() {
         Scanner input = new Scanner(System.in);
         return input.nextLine() + ".txt";
     }
 
-        /*
-        System.out.print("Please select a name for your file (i'll take care of the file type (:\n>");
-        String fileName = createTextFile();
-        String fileLocation = "C:\\Users\\Mathi\\Desktop\\" + fileName + ".txt";
-        BufferedWriter writer = new BufferedWriter(
-                new FileWriter(fileLocation)
-        );
-        System.out.println(badName);
-        writer.write(String.valueOf(badName));
-        writer.close();
 
-         */
 
     public void welcome() {
         System.out.println("Welcome to storyBuilder.v1");
