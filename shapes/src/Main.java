9bb250b7c4cc47c546b088e977378966de954aa5
KidRonee <mabr075@student.kristiania.no>
2021-02-17 00:47:50 +0100

    added changes

--- shapes/src/Main.java
+++ shapes/src/Main.java
@@ -2,6 +2,7 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
+        /*
         Shape shape = new Shape();
         shape.setColor(Color.pink);
         Rectangle rect = new Rectangle();
@@ -9,6 +10,30 @@ public class Main {
         rect.setWidth(20);
         rect.setFilled(true);
         rect.printState();
+         */
+        for (int x = 0; x < 6; x++) {
+            for (int y = 0; y < 6; y++) {
+                if (x == y) {
+                    System.out.print("|||");
+                }
+                if (x == 1 && y == 5) {
+                    System.out.println("|||");
+                }
+                if (x == 2 && y == 4) {
+                    System.out.println("|||");
+                }
+                if (x == 4 && y == 2) {
+                    System.out.println("|||");
+                }
+                if (x == 5 && y == 1) {
+                    System.out.println("|||");
+                }
+                else {
+                    System.out.println("---");
+                }
+            }
+            System.out.println();
+        }
 
 
     }
--- ticTacToe/src/Main.java
+++ ticTacToe/src/Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
         TTT game = new TTT();
@@ -29,6 +30,4 @@ public class Main {
             System.out.println(Character.toUpperCase(game.getCurrentPlayerMark()) + " Wins!");
         }
     }
-
-
 }
